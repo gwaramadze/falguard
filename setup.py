26a9789510f553d688946ff26e0cd34dc25f7706
Remy Gwaramadze <remy@gwaramadze.com>
2018-12-29 14:32:01 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='falguard',
-    version='0.1',
+    version='0.2',
     author='Remy Gwaramadze',
     author_email='remy@gwaramadze.com',
     url='https://github.com/gwaramadze/falguard',
